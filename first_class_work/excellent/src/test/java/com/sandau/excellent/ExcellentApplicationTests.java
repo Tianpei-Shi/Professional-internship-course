198ebc16f4d81d44dd089973894e8a0522494891
史天培 <2441093931@qq.com>
2024-11-21 17:55:22 +0800

    Changes to be committed: 	new file:   first_class_work/excellent/.gitattributes 	new file:   first_class_work/excellent/.gitignore 	new file:   first_class_work/excellent/.mvn/wrapper/maven-wrapper.properties 	new file:   first_class_work/excellent/mvnw 	new file:   first_class_work/excellent/mvnw.cmd 	new file:   first_class_work/excellent/pom.xml 	new file:   first_class_work/excellent/src/main/java/com/sandau/excellent/ExcellentApplication.java 	new file:   first_class_work/excellent/src/main/resources/application.properties 	new file:   first_class_work/excellent/src/test/java/com/sandau/excellent/ExcellentApplicationTests.java

---
+++ first_class_work/excellent/src/test/java/com/sandau/excellent/ExcellentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandau.excellent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcellentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
